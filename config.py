cc3d550b99aba4420428f67cf2686563f2587a6b
diongzy <86460920+diongzy@users.noreply.github.com>
2023-08-29 15:40:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cxtk9ixv8wey.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "diongzy-employee"
+customregion = "us-east-1"
+
